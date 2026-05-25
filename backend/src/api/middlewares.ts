38611c36ef8bee2ec08362022da808a443e50dab
avenegra07 <josedgcatala@gmail.com>
2026-05-26 00:31:34 +0200

    Refactor middlewares to include a logger function

--- backend/src/api/middlewares.ts
+++ backend/src/api/middlewares.ts
@@ -1,65 +1,24 @@
-import { defineMiddlewares } from "@medusajs/utils";
+import type {
+  MiddlewaresConfig,
+  MedusaRequest,
+  MedusaResponse,
+  MedusaNextFunction,
+} from "@medusajs/medusa";
 
-export default defineMiddlewares({
+async function logger(
+  req: MedusaRequest,
+  res: MedusaResponse,
+  next: MedusaNextFunction
+) {
+  console.log("Request received");
+  next();
+}
+
+export const config: MiddlewaresConfig = {
   routes: [
     {
-      matcher: "/",
-      middlewares: [
-        (req, res, next) => {
-          // CSP personalizada para resolver el bloqueo de scripts
-          res.setHeader(
-            "Content-Security-Policy",
-            `default-src 'self'; ` +
-            `script-src-elem 'self' 'unsafe-inline' 'sha256-ZswfTY7H35rbv8WC7NXBoiC7WNu86vSzCDChNWwZZDM=' https: http:; ` +
-            `script-src 'self' 'unsafe-inline' 'unsafe-eval' https: http:; ` +
-            `img-src 'self' data: https: http:; ` +
-            `style-src 'self' 'unsafe-inline' https: http:; ` +
-            `font-src 'self' data: https: http:; ` +
-            `connect-src 'self' https: http: ws: wss:; ` +
-            `frame-src 'self' https:;`
-          );
-          next();
-        },
-      ],
-    },
-    // Aplica a todas las rutas del admin y store
-    {
-      matcher: "/admin*",
-      middlewares: [
-        (req, res, next) => {
-          res.setHeader(
-            "Content-Security-Policy",
-            `default-src 'self'; ` +
-            `script-src-elem 'self' 'unsafe-inline' 'sha256-ZswfTY7H35rbv8WC7NXBoiC7WNu86vSzCDChNWwZZDM=' https: http:; ` +
-            `script-src 'self' 'unsafe-inline' 'unsafe-eval' https: http:; ` +
-            `img-src 'self' data: https: http:; ` +
-            `style-src 'self' 'unsafe-inline' https: http:; ` +
-            `font-src 'self' data: https: http:; ` +
-            `connect-src 'self' https: http: ws: wss:; ` +
-            `frame-src 'self' https:;`
-          );
-          next();
-        },
-      ],
-    },
-    {
-      matcher: "/store*",
-      middlewares: [
-        (req, res, next) => {
-          res.setHeader(
-            "Content-Security-Policy",
-            `default-src 'self'; ` +
-            `script-src-elem 'self' 'unsafe-inline' 'sha256-ZswfTY7H35rbv8WC7NXBoiC7WNu86vSzCDChNWwZZDM=' https: http:; ` +
-            `script-src 'self' 'unsafe-inline' 'unsafe-eval' https: http:; ` +
-            `img-src 'self' data: https: http:; ` +
-            `style-src 'self' 'unsafe-inline' https: http:; ` +
-            `font-src 'self' data: https: http:; ` +
-            `connect-src 'self' https: http: ws: wss:; ` +
-            `frame-src 'self' https:;`
-          );
-          next();
-        },
-      ],
+      matcher: "/store/custom",
+      middlewares: [logger],
     },
   ],
-});
+};
