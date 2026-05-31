2308080ab85ce8ecbc09db6231e30b7e228fa750
avenegra07 <josedgcatala@gmail.com>
2026-05-31 20:08:18 +0200

    Update medusa-config.js

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -91,10 +91,12 @@ const medusaConfig = {
         }
       }
     }] : []),
+    ...(BOOKING ? [{
      {
       resolve: "@rsc-labs/medusa-booking-system",
       options: {},
     },
+    }] : []),
     ...(SENDGRID_API_KEY && SENDGRID_FROM_EMAIL || RESEND_API_KEY && RESEND_FROM_EMAIL ? [{
       key: Modules.NOTIFICATION,
       resolve: '@medusajs/notification',
